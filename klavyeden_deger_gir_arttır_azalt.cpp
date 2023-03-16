58d96145f9dfea2d9a8f12ac270212cfe294c3d9
Ataberk koç <ataberkoc123@outlook.com>
2023-03-16 14:20:21 +0300

    Create klavyeden_deger_gir_arttır_azalt.cpp
    Klavye girişli sayı ekle azalt

---
+++ klavyeden_deger_gir_arttır_azalt.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	
+	cout<<"temel girdi islemleri"<<endl;
+	
+	
+	int a,b;
+	
+	cout<<"lutfen ilk sayiyi giriniz"<<endl;
+		cin>> a;
+	cout<<"lutfen ikinci sayiyi giriniz"<<endl;
+		cin>> b;
+	
+	
+	cout<<"klavyeden girdiginiz ilk deger"<<a<<endl<<"klavyeden girdiginiz ikinci deger"<<b<<endl;
+	cout<<"klavyeden girlen ik degerin +10 fazlasi"<<a+10<<endl;
+	cout<<"klavyeden girlen ikinci degerin -10 fazlasi"<<b-10<<endl;
+	
+	
+}
